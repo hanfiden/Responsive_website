6bcd8f834702bf78a18134736d151a7831c41435
han fiden <fidendu2@gmail.com>
2021-06-01 23:44:24 +0200

    Complete functional php form

--- contactform.php
+++ contactform.php
@@ -1,14 +1,20 @@
-<? php
+<?php
 
 if(isset($_POST['submit'])) {
-	g$mailFrom = $_POST['email'];
+	$mailFrom = $_POST['email'];
+	$subject = $_POST['subject'];
 	$name = $_POST['name'];
 	$message = $_POST['message'];
 
-	$mailTo = "fidenhan@gmail.com";
+	$mailTo = 'fidenhan@gmail.com';
 	$headers = "From: ".$mailFrom;
-	$txt = "You have received an email from ".$name.".\n\n".$message;
+	$txt = "You have received an email from ".$name.".\n".$message.".\n"."Sender's email: ".$mailFrom;
 
-	mail($mailTo, $txt, $headers);
-	header("Location: index.html?mailsent");
+	if(mail($mailTo, $subject, $txt, $headers)) {
+		header('Location: index.html?Message_sent!');
+	} else {
+		header('Location: index.html?Failed_to_send!');
+	};
 }
+
+?>
